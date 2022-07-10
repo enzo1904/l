a478185abc83d9d5b3782274cd9370a73680ec64
enzo1904 <101226267+enzo1904@users.noreply.github.com>
2022-07-10 01:20:54 -0400

    ñ

---
+++ jquery.js
@@ -0,0 +1,68 @@
+$("formulario_ct").validate({
+    rules:{
+        nombre_contacto:{
+            required: true,
+            minlength: 4,
+            maxlength: 30
+        },
+        email_contacto:{
+            required: true,
+            email: true
+        },
+        telefono_contacto:{
+            required: true,
+            telefono_contacto: {matches:"[0-9]+",minlength:9, maxlength:9}
+        },
+        mensaje_contacto:{
+            required: true,
+            minlength: 5,
+            maxlength: 200
+        }
+    }
+})
+
+
+
+
+
+$("#btn_ct").click(function(){
+    if($("#formulario_contacto").valid() == false){
+        return;
+    }
+    let nombre_contacto = $("#nombre_contacto").val()
+    let email_contacto = $("#email_contacto").val()
+    let telefono_contacto = $("telefono_contacto").val()
+    let mensaje_contacto = $("mensaje_contacto").val()
+    let avisos_contacto = $("avisos_contactos").is("checked")
+})
+
+
+
+
+
+
+
+
+$("#formulario_contacto").validate({
+    rules:{
+        email:{
+            required: true,
+            email:true
+        },
+        password:{
+             required: true,
+             minlength: 4,
+             maxlength: 14
+        }
+    }
+})
+
+$("#btn").click(function(){
+    if( $("#formulario_contacto").valid()==false){
+        return;
+    }
+  let email = $("#email").val()
+  let password = $("#password").val()
+})
+
+
